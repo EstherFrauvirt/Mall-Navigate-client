3b10e2e626bcba31671c2e57541081ce925e3f30
unknown <chagit2002@gmail.com>
2023-12-26 13:07:51 +0200

    third-chagit

--- navigation-client/src/components/Login.jsx
+++ navigation-client/src/components/Login.jsx
@@ -3,6 +3,8 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useNavigate, Link } from "react-router-dom";
 import Alert from 'react-bootstrap/Alert';
+import config from './config';
+
 
 
 
@@ -15,12 +17,32 @@ export default function Login() {
   const navigate = useNavigate();
 
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`${config.BASE_URL}users/login`,
+        {
+          method: 'POST', 
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(user),
+        });
 
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      lcosole.log(error);
+    }
+  };
 
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
+    fetchData();
+
   };
 
   return (
@@ -39,13 +61,13 @@ export default function Login() {
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" onChange={(e) => { setUser({ ...user, password: e.target.value }) }} />
         </Form.Group>
-        
+
         <Button variant="primary" type="submit" onClick={handleSubmit}>
           Submit
       </Button>
-      <br />
-      <br />
-      <Alert.Link href="/register">new user? register</Alert.Link>
+        <br />
+        <br />
+        <Alert.Link href="/register">new user? register</Alert.Link>
       </Form>
     </>
   );
