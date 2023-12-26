08aa010576be0fb6bcef826787b0097ffedc0f3a
unknown <chagit2002@gmail.com>
2023-12-26 11:09:55 +0200

    second-chagit

--- navigation-client/src/components/Login.jsx
+++ navigation-client/src/components/Login.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useContext } from 'react'
-import { useNavigate } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useNavigate, Link } from "react-router-dom";
+import Alert from 'react-bootstrap/Alert';
 
 
 
@@ -12,14 +15,38 @@ export default function Login() {
   const navigate = useNavigate();
 
 
+
+
+
   const handleSubmit = (e) => {
+    e.preventDefault();
     
   };
 
   return (
     <>
       <h5>LOGIN</h5>
-      
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" onChange={(e) => { setUser({ ...user, email: e.target.value }) }} />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+        </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" onChange={(e) => { setUser({ ...user, password: e.target.value }) }} />
+        </Form.Group>
+        
+        <Button variant="primary" type="submit" onClick={handleSubmit}>
+          Submit
+      </Button>
+      <br />
+      <br />
+      <Alert.Link href="/register">new user? register</Alert.Link>
+      </Form>
     </>
   );
 }
--- navigation-client/src/components/Register.jsx
+++ navigation-client/src/components/Register.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { Button, Form } from 'react-bootstrap'
+import { useNavigate } from "react-router-dom";
+
 
 export default function Register() {
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    });
+    const navigate = useNavigate();
+
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate("/");
+    };
+
     return (
         <div>
-            
-        </div>
+            <h5>REGISTER</h5>
+
+            <Form>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" onChange={(e) => { setUser({ ...user, email: e.target.value }) }} />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" onChange={(e) => { setUser({ ...user, password: e.target.value }) }} />
+                </Form.Group>
+
+
+                <Button variant="primary" type="submit" onClick={handleSubmit}>
+                    Submit
+                </Button>
+            </Form></div>
     )
 }
