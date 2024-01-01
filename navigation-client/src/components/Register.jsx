4d74c8777c27ae712555c7c703df7a3646c15171
EstherFrauvirt <esty2048@gmail.com>
2024-01-01 09:53:37 +0200

    register design

--- navigation-client/src/components/Register.jsx
+++ navigation-client/src/components/Register.jsx
@@ -2,9 +2,19 @@ import React, { useState, useContext } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import { useNavigate } from "react-router-dom";
 import config from './config';
+import { Box, Card, CardActions, CardContent,TextField, Typography } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 
 
 export default function Register() {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
     const [user, setUser] = useState({
         email: '',
         password: '',
@@ -45,33 +55,51 @@ export default function Register() {
     };
 
     return (
-        <div>
-            <h5>REGISTER</h5>
-
-            <Form>
-
-                <Form.Group className="mb-3" controlId="formBasicName">
-                    <Form.Label>name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter your name" onChange={(e) => { setUser({ ...user, name: e.target.value }) }} />
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" onChange={(e) => { setUser({ ...user, email: e.target.value }) }} />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" onChange={(e) => { setUser({ ...user, password: e.target.value }) }} />
-                </Form.Group>
+      <div style={{height:'90vh'}}>
 
 
-                <Button variant="primary" type="submit" onClick={handleSubmit}>
-                    Submit
-                </Button>
-            </Form></div>
+      <Card sx={{ minWidth: 275 ,width:"40%", marginLeft:'30%',marginTop:'',position:'absolute',top:'25%', padding:'20px'}}>
+            <Box display="flex" flexDirection="column" alignItems="center">      <CardContent>
+              <Typography sx={{ fontSize: 40 }} color="#4a4cf5" textAlign={'center'} >
+                REGISTER
+              </Typography> <br/><br/>
+             
+              <Form>
+              <TextField
+              style={{ width: '310px' }}
+              label="Name" variant="outlined" type='text' onChange={(e) => { setUser({ ...user, name: e.target.value }) }} />
+              <br/><br/>
+              <TextField
+              style={{ width: '310px' }}
+              label="Email address" variant="outlined" type='email' onChange={(e) => { setUser({ ...user, email: e.target.value }) }} />
+              <br/><br/>
+              <TextField
+              onChange={(e) => { setUser({ ...user, password: e.target.value }) }} 
+              style={{ width: '310px' }}
+            label="Password"
+            type={showPassword ? 'text' : 'password'}
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={handleTogglePassword} edge="end">
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+      <br/>
+               
+      
+                <br />
+              </Form>
+            </CardContent>
+            <CardActions>
+              <Button sx={{color:'#4a4cf5'}} size="medium" onClick={handleSubmit} variant='outlined'>Submit</Button>
+            </CardActions>
+            </Box>
+          </Card>
+         </div>
     )
 }
