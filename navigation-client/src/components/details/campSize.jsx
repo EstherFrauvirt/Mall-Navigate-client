480b469d13964e07a62ca4cf8eae7e9fbcec4403
EstherFrauvirt <esty2048@gmail.com>
2024-01-02 10:31:44 +0200

    campsize change and merge

--- navigation-client/src/components/details/campSize.jsx
+++ navigation-client/src/components/details/campSize.jsx
@@ -6,9 +6,44 @@ import { Link } from 'react-router-dom'
 
 export default function CampSize() {
   const { setHeight, setWidth, height, width } = useContext(MallContext);
+
+  const [hightError, setHightError] = useState(false);
+  const [widthError, setWidthError] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (hightError) {
+      alert("hight is not valid")
+    }
+    else if (widthError) {
+      alert("width is not valid")
+    }
+    else {
+      navigate(`/buildMatrix?height=${height}&width=${width}&mall=${state}`)
+      // You can use height and width values as needed (e.g., send them to the server, perform calculations, etc.)
+      console.log('Height:', height, 'Width:', width);
+    }
     console.log('Height:', height, 'Width:', width);
+  };
+
+  const handleWidthChange = (event) => {
+    const value = event.target.value;
+
+    // Validate that the input is an integer
+    const isValidWidth = /^\d+$/.test(value);
+
+    setWidthError(!isValidWidth);
+    setWidth(value);
+  };
+
+  const handleHightChange = (event) => {
+    const value = event.target.value;
+
+    // Validate that the input is an integer
+    const isValidHight = /^\d+$/.test(value);
+
+    setHightError(!isValidHight);
+    setHeight(value);
   };
 
   return (
@@ -20,30 +55,42 @@ export default function CampSize() {
           </Typography> <br /><br />
 
         
-            <TextField
-             margin="dense"
-             required
-             id="outlined-required"
-             label="Hight"// טעות החלפתי בין משתנים
-             name="height"
-             defaultValue=""
-             onChange={(e) => { setHeight(e.target.value), handleHightChange(e) }}
-             error={hightError}
-             helperText={hightError ? 'Please enter a valid integer' : ''}
-              style={{ width: '310px' }}
-              variant="outlined" type="number"
-              value={height}></TextField>
+          <TextField
+          margin="dense"
+          required
+          id="outlined-required"
+          label="Hight"// טעות החלפתי בין משתנים
+          name="height"
+          defaultValue=""
+          onChange={(e) => { setHeight(e.target.value), handleHightChange(e) }}
+          error={hightError}
+          helperText={hightError ? 'Please enter a valid integer' : ''}
+
+        />
+        <br />
+        <TextField
+          margin="dense"
+          required
+          id="outlined-required"
+          label="Width"// טעות החלפתי בין משתנים
+          name="width"
+          defaultValue=""
+          onChange={(e) => { setWidth(e.target.value), handleWidthChange(e) }}
+          error={widthError}
+          helperText={widthError ? 'Please enter a valid integer' : ''}
+
+        />
     
-            <TextField
+            {/* <TextField
               type="number"
               value={width}
               onChange={(e) => setWidth(e.target.value)}
               style={{ width: '310px' }}
-              variant="outlined" />
+              variant="outlined" /> */}
     
         </CardContent>
           <CardActions>
-            <Link to={"/admin/campSize"}> <Button sx={{ color: '#4a4cf5' }} size="medium"  variant='outlined'>Continue</Button></Link>
+            <Link to={"/admin/campSize"}> <Button sx={{ color: '#4a4cf5' }} size="medium"  variant='outlined' onClick={handleSubmit}>Continue</Button></Link>
           </CardActions>
         </Box>
       </Card>
