957430ab90c26d199f58d7ec408e67462c95a5c8
unknown <chagit2002@gmail.com>
2024-01-03 14:43:08 +0200

    vgrn

--- navigation-client/src/components/routers/appRouters.jsx
+++ navigation-client/src/components/routers/appRouters.jsx
@@ -14,30 +14,47 @@ import Footer from '../footer'
 import Navbar from '../navBar';
 import GlobalModal from '../modal'
 import Erase from '../details/erase'
+import { getUserLocation } from '../utils/map'
+import { findClosestCoordinate } from '../utils/map'
 
 import EmailComponent from '../../components/email'
+import Maps from '../../components/Maps'
+
+
+const mallCoords = [
+
+[31.0981, 34.8783],
+[31.7380, 34.6943]
+
+];
+
+const startCoord = await getUserLocation();
+const endCoord = await findClosestCoordinate(startCoord, mallCoords);
+console.log(endCoord);
 
 
 export default function AppRoutes() {
     return (
         <div>
-        <Navbar></Navbar>
+            <Navbar></Navbar>
 
             <Routes>
-                <Route path='/admin' element={<Admin/>}>
+                <Route path='/admin' element={<Admin />}>
                     <Route path='buildMatrix' element={<BuildMatrix />} />
-                   <Route path='campSize' element={<CampSize  />} />
+                    <Route path='campSize' element={<CampSize />} />
                 </Route>
-                
-                 <Route path='/erase' element={<Erase/>} />
-                 <Route path='/create' element={<CreatePath/>} />
-                 <Route path='/' element={<Home />}/>
-                 <Route path='/register' element={<Register />}/>
-                 <Route path='/login' element={<Login />}/>
-                 <Route path='/email' element={<EmailComponent />}/>
+
+                <Route path='/erase' element={<Erase />} />
+                <Route path='/create' element={<CreatePath />} />
+                <Route path='/' element={<Home />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/email' element={<EmailComponent />} />
+                <Route path='/maps' element={<Maps startCoords={startCoord} endCoords={endCoord} />} />
 
 
-               {/* <Route path='/cvList' element={<CvList />} /> */}
+
+                {/* <Route path='/cvList' element={<CvList />} /> */}
                 {/* <Route path='/cvList/:id' element={<Resume />} /> */}
             </Routes>
             <GlobalModal></GlobalModal>
